636f9e6bd4782bc38abe51ed4eb720db1a0835a4
Mehrdad <87481468+MehrdadWeb@users.noreply.github.com>
2023-09-08 07:38:44 +0330

    Update axios-orders.js

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://react-my-burger-3db24-default-rtdb.europe-west1.firebasedatabase.app/"
+    baseURL: "https://react-my-burger-4e1ad-default-rtdb.firebaseio.com/"
+    // baseURL: "https://react-my-burger-3db24-default-rtdb.europe-west1.firebasedatabase.app/"
 })
 
 export default instance;
