86bfe87cd80f0e09c1bcab51f52e7427b74fe4a1
Mehrdad <87481468+MehrdadWeb@users.noreply.github.com>
2023-09-08 07:50:38 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -12,21 +12,28 @@ import burgerBuilderReducer from "./store/reducers/burgerBuilder";
 import orderReducer from "./store/reducers/order";
 import authReducer from "./store/reducers/auth";
 
-const composeEnhancers = process.env.NODE_ENV === "development" ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+// const composeEnhancers = process.env.NODE_ENV === "development" ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
     order: orderReducer,
     auth: authReducer
+    // => state = {burgerBuilder:burgerBuilderReducer,order:orderReducer} => state.burgerBuilder & state.order
+
 })
 
+// اضافه می شدند applyMiddleware ها به middleware می باشد به پروژه ما اضافه شده است. middleware که یک thunk
+// به ما کمک می کند که thunk
+// یک تابع را برگردانیم action به جای برگرداندن
 const store = createStore(
-                            rootReducer,
-                            composeEnhancers(
-                                applyMiddleware(thunk)
-                            )
-                        );
-
+    rootReducer,
+    composeEnhancers(
+        applyMiddleware(thunk)
+    )
+);
+// const store = createStore(rootReducer);
+//const store = createStore(reducer);
 
 ReactDOM.render(
     <Provider store={store}>
@@ -37,3 +44,4 @@ ReactDOM.render(
     document.getElementById('root')
 );
 registerServiceWorker();
+
