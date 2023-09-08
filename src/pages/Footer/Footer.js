0c39680204adde70796e02efbf4ebfe0f2d2d566
Mehrdad <87481468+MehrdadWeb@users.noreply.github.com>
2023-09-08 08:02:48 +0330

    Update Footer.js

--- src/pages/Footer/Footer.js
+++ src/pages/Footer/Footer.js
@@ -1,1 +1,45 @@
+import React from "react";
+// import { FaGithub } from "react-icons/fa";
+
+const styles = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  flexDirection: "column",
+  padding: "25px",
+  background: "#FFFCF8",
+};
+
+const Footer = () => {
+  return (
+    <div style={styles}>
+      <p>
+        you made tasty 🍔, loved it ?{" "}
+        <span>
+          add some new features -{" "}
+          <a href="">
+            {<div
+              style={{
+                fontSize: "1.5em",
+                cursor: "pointer",
+                marginBottom: "-5px",
+              }}
+            />}
+          </a>{" "}
+        </span>
+      </p>
+      <h3>
+        Developed with ❤️ by{" "}
+        <a
+          href="https://github.com/MehrdadWeb"
+          style={{ textDecoration: "none", borderBottom: "2px solid #E8833E" }}
+        >
+          Mehrdad Mohammadi
+        </a>{" "}
+      </h3>
+    </div>
+  );
+};
+
+export default Footer;
 
